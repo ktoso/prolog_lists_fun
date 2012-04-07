25defcf488d2613b7785274c42957b5faea00ac4
Konrad Malawski <konrad.malawski@project13.pl>
2012-04-07 22:52:13 +0200

    started zad7

---
+++ zad7.pl
@@ -0,0 +1,19 @@
+% zdefiniować predykat przeloz(L1,L2), który zamienia listę liczb (max. 0-9), na listę słów:
+%
+% ?- przeloz([1,4,7],X).
+% X = [jeden, cztery, siedem] ;
+%
+% ?- przeloz(A,[dwa,osiem,zero]).
+% A = [2, 8, 0] ;
+%
+% posługując się faktami:
+%
+znaczy(0,zero).   znaczy(1,jeden).
+znaczy(2,dwa).    znaczy(3,trzy).
+znaczy(4,cztery). znaczy(5,piec).
+znaczy(6,szesc).  znaczy(7,siedem).
+znaczy(8,osiem).  znaczy(9,dziewiec).
+
+% impl
+
+
